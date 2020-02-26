a557bcabee3b5a66b9e08d076e792e5b737fbca8
Pranav Konda <theotaku196@gmail.com>
2020-02-26 18:52:19 -0500

    2262020

--- src/main/java/frc/robot/commands/drivetrain/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/drivetrain/ArcadeDrive.java
@@ -43,7 +43,7 @@ public class ArcadeDrive extends CommandBase {
   public void execute() {
     double speedMultiplierRaw = accelerationSupplier.getAsDouble();
     double throttle = throttleSupplier.getAsDouble();
-    double turn = Math.pow(turnSupplier.getAsDouble(), 3);
+    double turn = Math.pow(turnSupplier.getAsDouble(), 5);
     
     double speedMultiplier = MathUtil.normalize(1, -1, 0.2, 1, speedMultiplierRaw);
     // System.out.println("Speed Multiplier " + speedMultiplier);
